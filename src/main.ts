c90f67032a96f5950f1262dab116cc030c32744d
Kush Choudhary <kushc225@gmail.com>
2026-09-20 20:32:45 +0530

    feat : testing

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+  
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 await bootstrap();
